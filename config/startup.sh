46f57b68444d87b65e215342dd2f6c6383901791
JamesGreenaway <16066511+JamesGreenaway@users.noreply.github.com>
2019-07-27 00:20:01 +0100

    change write permissions back and reordered chown

--- config/startup.sh
+++ config/startup.sh
@@ -9,8 +9,6 @@ echo -e "\n[tls]\n  [[tls.certificates]]\n    certFile = \"/certs/$COMPOSE_PROJE
 if [ -d /var/www/html/vendor/ ]; then
     echo '- Craft project already created.'
 else
-    sudo chown -R craft:www-data /var/www/html/
-    sudo chmod g+s /var/www/html/
     setup_mysql_database () {
         mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h${MYSQL_HOSTNAME:-mysql} -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}"
         mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h${MYSQL_HOSTNAME:-mysql} -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_USER}@'%'"
@@ -23,10 +21,10 @@ else
         composer create-project craftcms/craft /var/www/html/
         ./craft setup/security-key
         setup_mysql_database
+        sudo chmod -R g+w config web storage vendor .env composer.json composer.lock
         ./craft setup/db-creds --interactive=0 \
         --server=${MYSQL_HOSTNAME:-mysql} --database=${MYSQL_DATABASE} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --port=${MYSQL_PORT:-3306} --driver=mysql --table-prefix=${DATABASE_TABLE_PREFIX}
         ./craft install --interactive=0 --email=${EMAIL_ADDRESS} --username=${USER_NAME} --password=${PASSWORD} --site-name=${COMPOSE_PROJECT_NAME} --site-url=${DEFAULT_SITE_URL}
-        sudo chmod -R g+w config web storage vendor .env composer.json composer.lock
     else 
         echo '- Existing Craft project found! Installing...'
         composer update
@@ -34,8 +32,9 @@ else
         setup_mysql_database
         ./craft setup/db-creds --interactive=0 \
         --server=${MYSQL_HOSTNAME:-mysql} --database=${MYSQL_DATABASE} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --port=${MYSQL_PORT} --driver=mysql --table-prefix=${DATABASE_TABLE_PREFIX}
-        sudo chmod -R g+w config web storage vendor .env composer.json composer.lock
    fi
+   sudo chown -R craft:www-data /var/www/html/
+   sudo chmod g+s /var/www/html/
 fi
 
 if [ -f /tmp/virtualhost.conf ]; then
