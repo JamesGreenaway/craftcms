daa172059d3c3f6106c4e579a858bd3c469f5274
James Greenaway <greenaway.james@gmail.com>
2019-07-28 07:20:35 +0100

    env var update; .toml support removed; bind for composer
    DEFAULT_SITE_URL change to just SITE_URL to avoid env var overlaps
    from craft.
    
    Remove automatic certificate .toml file creation to reduce uneces-
    ary code in the future.
    
    Add bind volume to link to local .composer cache
    
    Refactor startup.sh
    
    Remove security key creation for Craft migration

--- config/startup.sh
+++ config/startup.sh
@@ -1,49 +1,69 @@
 #!/bin/bash
 set -e
 
-if [ -f /tmp/traefik/$COMPOSE_PROJECT_NAME.toml ]; then
-    sudo rm /tmp/traefik/$COMPOSE_PROJECT_NAME.toml
-fi
-echo -e "\n[tls]\n  [[tls.certificates]]\n    certFile = \"/certs/$COMPOSE_PROJECT_NAME-cert.pem\"\n    keyFile = \"/certs/$COMPOSE_PROJECT_NAME-key.pem\"\n" | sudo tee /tmp/traefik/$COMPOSE_PROJECT_NAME.toml > /dev/null
+sudo sh -c "chmod g+s /var/www/html/ && chown craft:www-data /var/www/html/"
+
+sudo chmod 777 /home/craft/.composer/
 
 if [ -d /var/www/html/vendor/ ]; then
     echo '- Craft project already created.'
+
 else
-    sudo chmod g+s /var/www/html/
-    sudo chown -R craft:www-data /var/www/html/
+    echo '- Checking for existing project...'
+
     setup_mysql_database () {
         mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h${MYSQL_HOSTNAME:-mysql} -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}"
         mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h${MYSQL_HOSTNAME:-mysql} -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_USER}@'%'"
         mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h${MYSQL_HOSTNAME:-mysql} -e "FLUSH PRIVILEGES"
     }
 
-    echo '- Checking for existing project...'
     if [ ! "$(ls -A /var/www/html/)" ]; then
         echo '- No project found. Creating new instance of CraftCMS...'
+
         composer create-project craftcms/craft /var/www/html/
+
         ./craft setup/security-key
+
         setup_mysql_database
+
         ./craft setup/db-creds --interactive=0 \
-        --server=${MYSQL_HOSTNAME:-mysql} --database=${MYSQL_DATABASE} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --port=${MYSQL_PORT:-3306} --driver=mysql --table-prefix=${DATABASE_TABLE_PREFIX}
-        ./craft install --interactive=0 --email=${EMAIL_ADDRESS} --username=${USER_NAME} --password=${PASSWORD} --site-name=${COMPOSE_PROJECT_NAME} --site-url=${DEFAULT_SITE_URL}
-         echo "DEFAULT_SITE_URL=\"https://$DEFAULT_SITE_URL\"" >> /var/www/html/.env
+        --server=${MYSQL_HOSTNAME:-mysql} \
+        --database=${MYSQL_DATABASE} \
+        --user=${MYSQL_USER} \
+        --password=${MYSQL_PASSWORD} \
+        --port=${MYSQL_PORT:-3306} \
+        --driver=mysql \
+        --table-prefix=${DATABASE_TABLE_PREFIX}
+
+        ./craft install --interactive=0 \
+        --email=${EMAIL_ADDRESS} \
+        --username=${USER_NAME} \
+        --password=${PASSWORD} \
+        --site-name=${COMPOSE_PROJECT_NAME} \
+        --site-url="https://${SITE_URL}"
+
     else 
         echo '- Existing Craft project found! Installing...'
+
         composer update
-        ./craft setup/security-key
+
         setup_mysql_database
-        ./craft setup/db-creds --interactive=0 \
-        --server=${MYSQL_HOSTNAME:-mysql} --database=${MYSQL_DATABASE} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --port=${MYSQL_PORT} --driver=mysql --table-prefix=${DATABASE_TABLE_PREFIX}
+
    fi
-   sudo chmod -R g+w config vendor web/cpresources storage .env composer.json composer.lock 
+   echo '- Setting write permissions for PHP...'
+
+   sudo chmod -R g+w config vendor web/cpresources storage .env composer.json composer.lock
+
    sudo chown -R craft:www-data /var/www/html/
-fi
 
-if [ -f /tmp/virtualhost.conf ]; then
-    sudo cp /tmp/virtualhost.conf /etc/apache2/sites-available/000-default.conf
-else
-    sudo sed -ri "s!ServerName!ServerName ${DEFAULT_SITE_URL}!" /etc/apache2/sites-available/000-default.conf
 fi
+chown root:root /etc/apache2/sites-available/000-default.conf
 
-sudo sh -c "echo 'ServerName ${DEFAULT_SITE_URL}' >> /etc/apache2/apache2.conf"
+sudo sh -c "echo 'ServerName ${SITE_URL}' >> /etc/apache2/apache2.conf"
+
+PURPLE='\033[1;35m'
+PARTY_POPPER='🎉'
+
+echo -e "\n- Congratulations ${PARTY_POPPER}  your CraftCMS site ready to go! Please visit: ${PURPLE} https://${SITE_URL}\n"
+
 sudo apache2-foreground
