6964febc6bb84b1e4ba59d63334310b73fade6e6
James Greenaway <greenaway.james@gmail.com>
2019-08-07 14:00:57 +0100

    minor formatting, env vars, vhost auto-populated
    protocol removed from site url
    manually add data to env file
    virtualhost auto-populated

--- startup.sh
+++ startup.sh
@@ -10,7 +10,7 @@ if [ -f /home/craft/.composer/ ]; then
     sudo chmod -f 777 /home/craft/.composer/
 fi
 
-# Use vendor directory as indication of whether project has been fully installed.
+# Use vendor directory as indication of whether an existing project has been fully installed.
 if [ -d /var/www/html/vendor/ ]; then
     echo '- Craft project already created.'
 else
@@ -39,15 +39,15 @@ else
         --username=${USER_NAME} \
         --password=${PASSWORD} \
         --site-name=${COMPOSE_PROJECT_NAME} \
-        --site-url="https://${SITE_URL}" \
+        --site-url=${SITE_URL} \
         --language=${LANGUAGE}
     }
 
-# Check if there are any files in /var/www/html.
+    # Check if there are any files in /var/www/html.
     if [ ! "$(ls -A /var/www/html/)" ]; then
         echo '- No project found. Creating new instance of CraftCMS...'
 
-# Run Composer as user 'craft' to avoid 'do not run as super-user' warning. 
+        # Run Composer as user 'craft' to avoid 'do not run as super-user' warning. 
         composer create-project craftcms/craft /var/www/html/
         ./craft setup/security-key
         setup_mysql_database
@@ -58,20 +58,27 @@ else
         composer update
         setup_mysql_database
         setup_craft_database
-
-# Manually add site URL to .env file.
-        echo -e "\nDEFAULT_SITE_URL=\"https://$SITE_URL\"" >> /var/www/html/.env
+        # Manually add data to .env file.
+        echo -e "\nSECURITY_KEY=\"$SECURITY_KEY\"" >> /var/www/html/.env
+        echo -e "\nENVIRONMENT=\"dev\"" >> /var/www/html/.env
+        echo -e "\nDEFAULT_SITE_URL=\"$SITE_URL\"" >> /var/www/html/.env
    fi
    echo '- Setting write permissions for PHP...'
    sudo chmod -R g+w config vendor web/cpresources storage .env composer.json composer.lock
 
-# Give group access to www-data for all files.
+   # Give group access to www-data for all files.
    sudo chown -R craft:www-data /var/www/html/
 fi
-sudo sh -c "echo 'ServerName ${SITE_URL}' >> /etc/apache2/apache2.conf"
+
+# Remove https from SITE_URL.
+export SERVER_NAME=`echo $SITE_URL |  awk -F"/" '{print $3}'` 
+sudo sh -c "echo 'ServerName ${SERVER_NAME}' >> /etc/apache2/apache2.conf"
+# Update Virtual Hosts with server name/alias.
+sudo sed -ri "s!ServerName!ServerName ${SERVER_NAME}!" /etc/apache2/sites-available/000-default.conf
+sudo sed -ri "s!ServerAlias!ServerAlias www.${SERVER_NAME}!" /etc/apache2/sites-available/000-default.conf
 
 PURPLE='\033[1;35m'
 PARTY_POPPER='🎉'
-echo -e "\n- Congratulations ${PARTY_POPPER}  your CraftCMS site ready to go! Please visit: ${PURPLE} https://${SITE_URL}\n"
+echo -e "\n- Congratulations ${PARTY_POPPER}  your CraftCMS site ready to go! Please visit: ${PURPLE} ${SITE_URL}\n"
 
 sudo apache2-foreground
