aeba7604e77e290de869b00de1c2b37028f52eb5
James Greenaway <greenaway.james@gmail.com>
2019-08-10 02:03:32 +0100

    add SIGTERM support + minor formatting

--- startup.sh
+++ startup.sh
@@ -1,87 +1,93 @@
 #!/bin/bash
 set -e
 
-# Change permissions, host mounted volume adds directory as root.
-sudo sh -c "chmod g+s /var/www/html/ && chown craft:www-data /var/www/html/"
+trap "exit 0" SIGTERM
 
-# Use vendor directory as indication of whether an existing project has been fully installed.
-if [ -d /var/www/html/vendor/ ]; then
-    echo '- Craft project already created.'
-else
-    echo '- Checking for existing project...'
+    # Change permissions, host mounted volume adds directory as root.
+    sudo sh -c "chmod g+s /var/www/html/ && chown craft:www-data /var/www/html/"
 
-    mysql-connect-retry () {
-        while ! mysqladmin ping -u${MYSQL_USER} -h${MYSQL_HOST:-mysql} -p${MYSQL_PASSWORD} --silent; do
-            echo "- Awaiting response from MySQL..."
-            sleep 1
-        done
-    }
+    # Use vendor directory as indication of whether an existing project has been fully installed.
+    if [ -d /var/www/html/vendor/ ]; then
+        echo '- Craft project already created.'
+    else
+        echo '- Checking for existing project...'
 
-    setup_mysql_database () {
-        mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h${MYSQL_HOSTNAME:-mysql} -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}"
-        mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h${MYSQL_HOSTNAME:-mysql} -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_USER}@'%'"
-        mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h${MYSQL_HOSTNAME:-mysql} -e "ALTER USER ${MYSQL_USER}@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_PASSWORD}'"
-        mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h${MYSQL_HOSTNAME:-mysql} -e "FLUSH PRIVILEGES"
-    }
+        mysql_connect_retry () {
+            while ! mysqladmin ping -u${MYSQL_USER} -h${MYSQL_HOST:-mysql} -p${MYSQL_PASSWORD} --silent; do
+                echo "- Awaiting response from MySQL..."
+                sleep 1
+            done
+        }
 
-    setup_craft_database () {
-        ./craft setup/db-creds --interactive=0 \
-        --server=${MYSQL_HOSTNAME:-mysql} \
-        --database=${MYSQL_DATABASE} \
-        --user=${MYSQL_USER} \
-        --password=${MYSQL_PASSWORD} \
-        --port=${MYSQL_PORT:-3306} \
-        --driver=mysql \
-        --table-prefix=${DATABASE_TABLE_PREFIX}
-    }
-    
-    install_craft () {
-        ./craft install --interactive=0 \
-        --email=${EMAIL_ADDRESS} \
-        --username=${USER_NAME} \
-        --password=${PASSWORD} \
-        --site-name=${SITE_NAME} \
-        --site-url=${SITE_URL} \
-        --language=${LANGUAGE}
-    }
+        setup_mysql_database () {
+            mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h${MYSQL_HOSTNAME:-mysql} -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}"
+            mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h${MYSQL_HOSTNAME:-mysql} -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_USER}@'%'"
+            mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h${MYSQL_HOSTNAME:-mysql} -e "ALTER USER ${MYSQL_USER}@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_PASSWORD}'"
+            mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h${MYSQL_HOSTNAME:-mysql} -e "FLUSH PRIVILEGES"
+        }
 
-    # Check if there are any files in /var/www/html.
-    if [ ! "$(ls -A /var/www/html/)" ]; then
-        echo '- No project found. Creating new instance of CraftCMS...'
-        # Run Composer as user 'craft' to avoid 'do not run as super-user' warning. 
-        composer create-project craftcms/craft /var/www/html/
-        ./craft setup/security-key
-        mysql-connect-retry
-        setup_mysql_database
-        setup_craft_database
-        install_craft
-    else 
-        echo '- Existing Craft project found! Installing...'
-        composer update
-        mysql-connect-retry
-        setup_mysql_database
-        setup_craft_database
-        # Manually add data to .env file.
-        echo -e "\nSECURITY_KEY=\"$SECURITY_KEY\"" >> /var/www/html/.env
-        echo -e "\nENVIRONMENT=\"dev\"" >> /var/www/html/.env
-        echo -e "\nDEFAULT_SITE_URL=\"$SITE_URL\"" >> /var/www/html/.env
-   fi
-   echo '- Setting write permissions for PHP...'
-   sudo chmod -R g+w config vendor web/cpresources storage .env composer.json composer.lock
-   # Give group access to www-data for all files.
-   sudo chown -R craft:www-data /var/www/html/
-fi
+        setup_craft_database () {
+            ./craft setup/db-creds --interactive=0 \
+            --server=${MYSQL_HOSTNAME:-mysql} \
+            --database=${MYSQL_DATABASE} \
+            --user=${MYSQL_USER} \
+            --password=${MYSQL_PASSWORD} \
+            --port=${MYSQL_PORT:-3306} \
+            --driver=mysql \
+            --table-prefix=${DATABASE_TABLE_PREFIX}
+        }
+        
+        install_craft () {
+            ./craft install --interactive=0 \
+            --email=${EMAIL_ADDRESS} \
+            --username=${USER_NAME} \
+            --password=${PASSWORD} \
+            --site-name=${SITE_NAME} \
+            --site-url=${SITE_URL} \
+            --language=${LANGUAGE}
+        }
 
-# Remove https from SITE_URL.
-export SERVER_NAME=`echo $SITE_URL |  awk -F"/" '{print $3}'` 
-sudo sh -c "echo 'ServerName ${SERVER_NAME}' >> /etc/apache2/apache2.conf"
-# Update Virtual Hosts with server name/alias.
-sudo sed -ri "s!ServerName!ServerName ${SERVER_NAME}!" /etc/apache2/sites-available/000-default.conf
-sudo sed -ri "s!ServerAlias!ServerAlias www.${SERVER_NAME}!" /etc/apache2/sites-available/000-default.conf
+        # Check if there are any files in /var/www/html.
+        if [ ! "$(ls -A /var/www/html/)" ]; then
+            echo '- No project found. Creating new instance of CraftCMS...'
+            # Run Composer as user 'craft' to avoid 'do not run as super-user' warning. 
+            composer create-project craftcms/craft /var/www/html/
+            ./craft setup/security-key
+            mysql_connect_retry
+            setup_mysql_database
+            setup_craft_database
+            install_craft
+        else 
+            echo '- Existing Craft project found! Installing...'
+            composer update
+            mysql_connect_retry
+            setup_mysql_database
+            setup_craft_database
+            # Manually add data to .env file.
+            echo -e "\nSECURITY_KEY=\"$SECURITY_KEY\"" >> /var/www/html/.env
+            echo -e "\nENVIRONMENT=\"dev\"" >> /var/www/html/.env
+            echo -e "\nDEFAULT_SITE_URL=\"$SITE_URL\"" >> /var/www/html/.env
+        fi
+        echo '- Setting write permissions for PHP...'
+        sudo chmod -R g+w config vendor web/cpresources storage .env composer.json composer.lock
+        # Give group access to www-data for all files.
+        sudo chown -R craft:www-data /var/www/html/
+    fi
 
-PURPLE='\033[1;35m'
-NO_COLOR='\033[0m'
-PARTY_POPPER='🎉'
-echo -e "\n- Congratulations ${PARTY_POPPER}  your CraftCMS site ready to go! Please visit: ${PURPLE} ${SITE_URL} ${NO_COLOR}\n"
+    # Remove https from SITE_URL.
+    export SERVER_NAME=`echo $SITE_URL |  awk -F"/" '{print $3}'` 
+    sudo sh -c "echo 'ServerName ${SERVER_NAME}' >> /etc/apache2/apache2.conf"
+    # Update Virtual Hosts with server name/alias.
+    sudo sed -ri "s!ServerName!ServerName ${SERVER_NAME}!" /etc/apache2/sites-available/000-default.conf
+    sudo sed -ri "s!ServerAlias!ServerAlias www.${SERVER_NAME}!" /etc/apache2/sites-available/000-default.conf
 
-sudo apache2-foreground
+    PURPLE='\033[1;35m'
+    NO_COLOR='\033[0m'
+    PARTY_POPPER='🎉'
+    echo -e "\n- Congratulations ${PARTY_POPPER}  your CraftCMS site ready to go! Please visit: ${PURPLE} ${SITE_URL} ${NO_COLOR}\n"
+
+while true
+do
+    sudo apache2-foreground &
+    wait $!
+done
